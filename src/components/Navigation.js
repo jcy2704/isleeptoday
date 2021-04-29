3fbe8731232741a66d1aef4af90af0b1f91ebd35
Steven J Chung <stevenjchung12@gmail.com>
2021-04-28 21:55:37 -0500

    Finish side navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import * as FaIcons from 'react-icons/fa';
+import * as GrIcons from 'react-icons/gr';
+import '../styles/Navigation/Navigation.css';
+import NavData from '../helpers/NavData';
 
 const Navigation = ({ loggedInStatus, handleLogout, user }) => {
+  const [sidebar, setSidebar] = useState(false);
+  const [lower, setLower] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      const scrolled = document.scrollingElement.scrollTop;
+      if (scrolled >= 10) {
+        setLower(true);
+      } else {
+        setLower(false);
+      }
+    });
+  }, []);
+
+  const handleSidebar = () => setSidebar(!sidebar);
+
   const history = useHistory();
 
   const handleClick = () => {
@@ -26,15 +46,41 @@ const Navigation = ({ loggedInStatus, handleLogout, user }) => {
 
     return (
       <>
-        <Link to="/login">Log In</Link>
-        <Link to="/signup">Sign Up</Link>
+        <Link to="/login" onClick={handleSidebar}>Log In</Link>
+        <Link to="/signup" onClick={handleSidebar}>Sign Up</Link>
       </>
     );
   };
 
   return (
-    <nav className="position-fixed">
-      {isLoggedIn()}
+    <nav>
+      <div className="menu-info">
+        <div className={`hamburger w-100 d-flex justify-content-end position-fixed ${lower ? 'white-bg' : ''}`}>
+          <button type="button" className="open-icon open-close" onClick={handleSidebar}>
+            <FaIcons.FaBars />
+          </button>
+        </div>
+
+        <div className={sidebar ? 'sidebar-cont active position-fixed' : 'sidebar-cont position-fixed'}>
+          <ul className="sidebar-items">
+            <li className="close-btn d-flex justify-content-end">
+              <button type="button" className="close-icon open-close" onClick={handleSidebar}>
+                <GrIcons.GrClose />
+              </button>
+            </li>
+            {NavData.map(data => (
+              <li key={data.title} className={data.cName}>
+                <Link to={data.path} onClick={handleSidebar}>
+                  {data.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="d-flex flex-column">
+            {isLoggedIn()}
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
---
+++ src/helpers/NavData.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: 'Home',
+    path: '/',
+    cName: 'nav-text',
+  },
+  {
+    title: 'Listing',
+    path: '/listing',
+    cName: 'nav-text',
+  },
+  {
+    title: 'Bookmarks',
+    path: '/bookmarks',
+    cName: 'nav-text',
+  },
+];
